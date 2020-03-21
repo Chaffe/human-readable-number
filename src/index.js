44669f70e0dd2fa354103d1cb0f4b98e6a0e0af6
Chaffe <mudrogelov.beled@gmail.com>
2020-03-21 11:16:01 +0300

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
+const units = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+]
+
+const dozens = [
+  "",
+  "ten",
+  "twenty",
+  "thirty",
+  "forty",
+  "fifty",
+  "sixty",
+  "seventy",
+  "eighty",
+  "ninety"
+]
+
 module.exports = function toReadable (number) {
-  
+  if (number < 20) return units[number];
+
+  if (number >= 20 && number < 100) {
+    if (number % 10 == 0) return dozens[number / 10];
+    else return (dozens[Math.floor(number / 10)]) + " " + units[number % 10];
+  }
+
+  if (number > 99) {
+    if (number % 100 == 0) return (units[number / 100] + " " +"hundred");
+
+    else if (number % 10 == 0) {
+      return (units[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' + 
+      dozens[(Math.floor(number / 10)) % 10]);
+    }
+
+    else if (number % 100 < 20) {
+      return (units[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' + 
+      units[number % 100]);
+    }
+
+    else return (units[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' +
+    dozens[(Math.floor(number / 10)) % 10] + ' ' + units[number % 10])
+  }
 }
